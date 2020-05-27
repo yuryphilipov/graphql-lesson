90407717b90443b5c5129dd12db239856da2c3d0
Yury Philipov <yury.philipov@gmail.com>
2020-05-27 19:07:50 +0800

    lesson 8

--- server/app.js
+++ server/app.js
@@ -7,14 +7,17 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3005;
 
-mongoose.connect(
-  `mongodb+srv://Yury:gzxlhAhONmHRMbZH@graphql-tutorial-dsdrr.mongodb.net/graphql-tutorial?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose
+  .connect(
+    `mongodb+srv://Yury:gzxlhAhONmHRMbZH@graphql-tutorial-dsdrr.mongodb.net/graphql-tutorial?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
-const dbConnection = mongoose.connection;
-dbConnection.on("error", (err) => console.log(`connection error: ${err}`));
-dbConnection.once("open", () => console.log("connected to DB"));
+// const dbConnection = mongoose.connection;
+// dbConnection.on("error", (err) => console.log(`connection error: ${err}`));
+// dbConnection.once("open", () => console.log("connected to DB"));
 
 app.use(
   "/graphql",
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -90,6 +90,42 @@ const DirectorType = new GraphQLObjectType({
   }),
 });
 
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addDirector: {
+      type: DirectorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        const director = new Directors({
+          name: args.name,
+          age: args.age,
+        });
+        return director.save();
+      },
+    },
+    addMovie: {
+      type: MovieType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        directorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        const movie = new Movies({
+          name: args.name,
+          genre: args.genre,
+          directorId: args.directorId,
+        });
+        return movie.save();
+      },
+    },
+  },
+});
+
 const Query = new GraphQLObjectType({
   name: "Query",
   fields: {
@@ -124,4 +160,5 @@ const Query = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: Query,
+  mutation: Mutation,
 });
